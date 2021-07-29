e315973486c17a4a0255c3823649238182deb7b3
Magnus <mgsgde@gmail.com>
2021-07-29 08:12:10 +0200

    bug fix

--- backend/config.js
+++ backend/config.js
@@ -22,7 +22,10 @@ const PRODUCTION = {
   DATABASE: {
     client: 'mysql',
     connection: {
-      // insert your credentials
+      host: process.env.MYSQL_HOST,
+      user: process.env.MYSQL_USER,
+      password: process.env.MYSQL_PASSWORD,
+      database: process.env.MYSQL_DATABASE,
     },
   },
 
