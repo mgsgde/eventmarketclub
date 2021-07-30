31a8dcf4af2bf028b8d0acd8d20d6b32ae9e2889
Magnus <mgsgde@gmail.com>
2021-07-30 11:27:05 +0200

    edit contact email

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,13 +1,9 @@
 import React from 'react'
-import {
-  Button, Row, Col, Form, FormGroup, Input,
-} from 'reactstrap';
 
 const Footer = (props) => {
   return (
-    <footer class="mt-3 p-3" style={{textAlign: "right"}} >
-
-    <a class="text-muted" href="mailto:info@eventmarket.com"><i className="fas fa-envelope mr-2 green-font"></i>Email</a>
+    <footer class="mt-3 p-3" style={{textAlign: 'right'}} >
+      <a class="text-muted" href="mailto:info@eventmarketclub.com"><i className="fas fa-envelope mr-2 green-font"></i>Email</a>
     </footer>
   )
 }
